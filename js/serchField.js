3377fd214039536e8bf5bff382ada7ba9d0af193
Aleksandra Kula <aleksandra.kula@outlook.com>
2019-08-21 20:57:32 +0100

    add search field

---
+++ js/serchField.js
@@ -0,0 +1,26 @@
+$("#myInput").keyup(function() {
+  var currentValue = $(this).val().toLowerCase().trim();
+
+  // Get all cards
+  var allCards = $(".cards")
+
+  // Remove display none from all cards
+  allCards.show();
+  
+  // Filter cards that lowercase title don't start with search value
+  var nonMatchingCards = allCards.filter(function() {
+    var titleText = $(this).find(".titleGrapes").text().toLowerCase().trim();
+    return !titleText.startsWith(currentValue);
+  })
+
+  // Set display none to all filtered cards
+  nonMatchingCards.hide();
+ 
+  if(nonMatchingCards.length===allCards.length) {
+    $(".nothingFound").show();
+  } else {
+    $(".nothingFound").hide();
+  }
+});
+
+ 

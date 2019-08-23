923c7947bc251d3fbe77b8b685dedb7d89adc532
Aleksandra Kula <aleksandra.kula@outlook.com>
2019-08-23 13:30:24 +0100

    added locations to maps

--- js/showMoreButton.js
+++ js/showMoreButton.js
@@ -19,21 +19,113 @@ $(document).ready(function(){
      
      $(".bntMap").first().click(function(){
           initMap(0, 0, locations = [
-               {lat: -1, lng: -1},
-               {lat: 1, lng: 1},
+               {lat: 48, lng: 2},
+               {lat: 37, lng: -104},
+               {lat: 43, lng: 12},
+               {lat: -34, lng: -65},
+               {lat: -29, lng: 26},
+               {lat: 58, lng: -117},
            
           ]);
           $(".mapOverlay").show("medium");
      })
      
      $(".bntMap").eq(1).click(function(){
-          initMap(52, 21, locations = [
-               {lat: 52, lng: 21},
-               {lat: 53, lng: 22}, 
+          initMap(0, 0, locations = [
+               {lat: 37, lng: -104},
+               {lat: -31, lng: 142},
+               {lat: -28, lng: -70},
+               {lat: -34, lng: -65},
+               {lat: 43, lng: 12},
+               {lat: -29, lng: 26},
+               {lat: 48, lng: 2},
+               
           ]);
           $(".mapOverlay").show("medium");
      })
      
+     $(".bntMap").eq(2).click(function(){
+          initMap(0, 0, locations = [
+               {lat: 37, lng: -104},
+               {lat: 43, lng: 12},
+               {lat: 48, lng: 2},
+               {lat: -28, lng: -70},
+               {lat: -31, lng: 142},
+               {lat: -29, lng: 26},
+               {lat: 46, lng: 8},
+               
+          ]);
+          $(".mapOverlay").show("medium");
+     })
+
+     $(".bntMap").eq(3).click(function(){
+          initMap(0, 0, locations = [
+               {lat: -34, lng: -65},
+               {lat: 37, lng: -104},
+               {lat: -28, lng: -70},
+               {lat: -31, lng: 142},
+               {lat: -31, lng: 142},
+          ]);
+          $(".mapOverlay").show("medium");
+     })
+
+     $(".bntMap").eq(4).click(function(){
+          initMap(0, 0, locations = [
+               {lat: 43, lng: 12},
+          ]);
+          $(".mapOverlay").show("medium");
+     })
+
+     $(".bntMap").eq(5).click(function(){
+          initMap(0, 0, locations = [
+               {lat: 48, lng: 2},
+               {lat: 37, lng: -104},
+               {lat: -37, lng: 175},
+               {lat: 43, lng: 12},
+               {lat: -31, lng: 142},,
+               {lat: -28, lng: -70},
+               {lat: 49, lng: 7},
+
+          ]);
+          $(".mapOverlay").show("medium");
+     })
+
+     $(".bntMap").eq(6).click(function(){
+          initMap(0, 0, locations = [
+               {lat: -29, lng: 26},
+          ]);
+          $(".mapOverlay").show("medium");
+     })
+
+     $(".bntMap").eq(7).click(function(){
+          initMap(0, 0, locations = [
+               {lat: -31, lng: 142},
+               {lat: -29, lng: 26},
+               {lat: 37, lng: -104},
+               {lat: -28, lng: -70},
+               {lat: -34, lng: -65},
+          ]);
+          $(".mapOverlay").show("medium");
+     })
+
+     $(".bntMap").eq(8).click(function(){
+          initMap(0, 0, locations = [
+               {lat: 38, lng: -3},
+               {lat: -31, lng: 142},
+               
+          ]);
+          $(".mapOverlay").show("medium");
+     })
+
+     $(".bntMap").eq(9).click(function(){
+          initMap(0, 0, locations = [
+               {lat: 37, lng: -104},
+               
+               
+          ]);
+          $(".mapOverlay").show("medium");
+     })
+
      $("#x").click(function(){
           $(".mapOverlay").hide("medium");
      })

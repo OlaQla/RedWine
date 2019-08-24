db43159a9dae8ee10b8f0a01ec8df58cdf4cab65
Aleksandra Kula <aleksandra.kula@outlook.com>
2019-08-24 21:16:39 +0100

    added basic age verification

--- js/ageVerify.js
+++ js/ageVerify.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 	var dayDropdown=$("#ageVerifyOverlay .btn.day");
 	var monthDropdown=$("#ageVerifyOverlay .btn.month");
 	var yearDropdown=$("#ageVerifyOverlay .btn.year");
+	var wrongInputText=$("#ageVerifyOverlay .wrongInput");
 
 	for (var i = 1; i <= 31; i++ ){
 		var newOption= $(`<option value=\"${i}\">${i}</option>`);
@@ -13,13 +14,38 @@ $(document).ready(function(){
 	}
 
 	$.each(months, function(index, value){
-		var newOption= $(`<option value="${index+1}">${value}</option>`);
+		var newOption= $(`<option value="${index}">${value}</option>`);
 		monthDropdown.append(newOption);
 	})
 
 	for (var i = 1939; i <= (new Date).getFullYear(); i++){
 		var newOption=$(`<option value=\"${i}\">${i}</option>`);
 		yearDropdown.append(newOption);
-	
 	}
+
+	$(".submit.btn").click(function(event){
+		event.preventDefault();
+		wrongInputText.hide();
+		var dayValue = parseInt(dayDropdown.val());
+		var monthValue = parseInt(monthDropdown.val());
+		var yearValue = parseInt(yearDropdown.val());
+		
+		if (dayValue === 0 || monthValue === 12 || yearValue === 0){
+			wrongInputText.show();
+		} else{
+			var dateValue = new Date(yearValue+18, monthValue, dayValue);
+			var currentDate = new Date();
+			var isOldEnough = dateValue.getTime() <= currentDate.getTime();
+
+			if (isOldEnough){
+				$("#ageVerifyOverlay").hide();
+			} else {
+				$(".tooYoung").show();
+				$(".wrapAgeVerify").hide();
+				var daysTo18 = Math.floor((dateValue - currentDate)/86400000);
+				$(".daysTo18").text(daysTo18);
+			}
+		}
+	})
+
 }) 

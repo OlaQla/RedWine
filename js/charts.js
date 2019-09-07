42a6170107a649d09e79359bba7a946079fc01f2
Aleksandra Kula <aleksandra.kula@outlook.com>
2019-09-07 22:04:41 +0100

    update css to align with scss, test chart path

--- js/charts.js
+++ js/charts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-d3.csv("../data/wine_production.csv", function (error, data) {
+d3.csv("data/wine_production.csv", function (error, data) {
 
     loadedData = data.map(d => { return { "Continent": d["Continent"], "Year": d["Year"], "Value": d["Value"] / 1000 }; });
     new d3plus.LinePlot()

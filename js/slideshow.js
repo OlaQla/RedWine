58cea714a94aa4814da85cb3a344085c289a04a3
Aleksandra Kula <aleksandra.kula@outlook.com>
2019-09-07 21:35:21 +0100

    fixed handling last element in carousel

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,29 +1,28 @@
 $(document).ready(function () {
-    // .slideshow -> set left to 0 -> $().css("left", 0)
-    $(".slideshow").css("left", 0);
-    // set timer func -> get left, if max set to 0 else set left to one you got + 100%
 
+    $(".slideshow").css("left", 0); // reset slide position
+
+    // function transitioning slides in a loop with 5s delay
     function moveSlide() {
         var slideshow = $(".slideshow");
         var leftvalue = slideshow.position().left;
-
         var firstImage = slideshow.children().first();
         var imageSize = firstImage.width();
-
-        console.log(leftvalue);
-
-        var maxwidth = slideshow.width();
-        console.log(maxwidth);
-
-        if (leftvalue - imageSize <= - maxwidth) {
+        var maxwidth = slideshow.width(); // total width of slides 
+       
+        /*transitioning from the last slide to the first one with no transition effect,
+         to make impression of having infinite number of slides*/
+        if (leftvalue - imageSize - 20 <= - maxwidth) { // 20 is put here to avoid rounding errors
+            slideshow.css("transition", "none");
             slideshow.css("left", 0);
+            setTimeout(moveSlide, 0)
+
+        // transition for all elements except the last one
         } else {
-
+            slideshow.css("transition", "left 2s");
             slideshow.css("left", leftvalue - imageSize);
+            setTimeout(moveSlide, 5000)
         }
-
-        setTimeout(moveSlide, 5000)
     }
-    
-    setTimeout(moveSlide, 5000)
+    setTimeout(moveSlide, 5000)// initial kick off of transitioning
 });

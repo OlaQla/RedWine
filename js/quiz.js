579f34a860f6117277d6c334dccf9a6e9a43c0ab
Aleksandra Kula <aleksandra.kula@outlook.com>
2019-09-20 11:40:47 +0100

    question update

--- js/quiz.js
+++ js/quiz.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     { question: "2/5 Which is NOT a variety of grape used for wine production?", answers: ["Merlot", "Pinot Noir", "Shiraz", "Shirazonnay"], correctId: 3 },
     { question: "3/5 The vintage date on a wine bottle indicate:", answers: ["The date the vineyard was founded", "The year the wine was botteled", "The year the grapes were picked", "The year the wine has been ranked"], correctId: 2 },
     { question: "4/5 How many calories are in 118ml of red wine?", answers: ["85 calories", "71 calories", "53 calories", "115 calories"], correctId: 0 },
-    { question: "5/5 About how meny litres of wine can be produced from 1ha of grapevines?", answers: ["270 liters", "840 liters", "2500 liters", "1450 liters"], correctId: 3 }];
+    { question: "5/5 About how many litres of wine can be produced from 1ha of grapevines?", answers: ["270 liters", "840 liters", "2500 liters", "1450 liters"], correctId: 3 }];
     var expert = 'You are an expert!';
     var wellDone = 'Well done!';
     var tryAgain = 'Try again...';
